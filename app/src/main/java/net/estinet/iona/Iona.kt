442e9e1cc32d4601621e26e936b0c499f21962fa
espidev <aguy867@gmail.com>
2017-02-02 17:05:31 -0500

    Changes

--- app/src/main/java/net/estinet/iona/Iona.kt
+++ app/src/main/java/net/estinet/iona/Iona.kt
@@ -3,13 +3,23 @@ package net.estinet.iona
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.media.MediaPlayer
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.support.v4.content.ContextCompat
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageView
+import java.io.File
+
+var systemDirectory: File = File("./")
 
 class Iona : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iona)
-        val mediaPlayer = MediaPlayer.create(super.getBaseContext(), R.raw.music)
-        mediaPlayer.start()
+        systemDirectory = getFilesDir()
+        //val mediaPlayer = MediaPlayer.create(super.getBaseContext(), R.raw.music)
+        //mediaPlayer.start()
     }
 }
