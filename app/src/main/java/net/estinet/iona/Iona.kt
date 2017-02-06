ef9bc9669543045b5f5482c3a5a7427ad59e2d2c
espidev <aguy867@gmail.com>
2017-02-06 12:24:52 -0500

    Changes

--- app/src/main/java/net/estinet/iona/Iona.kt
+++ app/src/main/java/net/estinet/iona/Iona.kt
@@ -19,45 +19,57 @@ var savedInstance: Bundle? = null
 class Iona : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //seshpenguin was here! 2017-02-02
         super.onCreate(savedInstanceState)
+
+        println("Starting Iona (Call from Iona Activity onCreate())")
 
         setContentView(R.layout.activity_iona)
         systemDirectory = getFilesDir()
         appCompatActivity = this
         savedInstance = savedInstanceState
 
+        println("Playing startup sound...")
+
         //Startup Sound
         val mediaPlayer = MediaPlayer.create(super.getBaseContext(), R.raw.music)
         mediaPlayer.start()
+
+        println("Starting fade animation for the Iona Logo...")
 
         val myImageView = findViewById(R.id.imageView) as ImageView
         val myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein)
         myImageView.startAnimation(myFadeInAnimation)
     }
     override fun onPostCreate(savedInstanceState: Bundle?){
-        Thread.sleep(5000)
-
+        super.onPostCreate(savedInstanceState)
+        println("Calling onPostCreate() for activity Iona...")
         val handler = Handler()
-        handler.postDelayed(Runnable {
+        handler.postDelayed({
             //Determine to launch setup or start program
+            println("Determining whether to launch or start setup...")
             val file = File(systemDirectory, "setup.txt")
             if(!file.exists()){
+                println("Setup file does not exist! Starting setup.")
                 file.createNewFile()
                 startSetup()
             }
             else{
-                var br: BufferedReader? = null
-                var fr: FileReader? = null
+                println("Setup file does exist. Checking setup stage...")
+                val fr: FileReader?
                 try {
                     fr = FileReader(file)
-                    br = BufferedReader(fr)
-                    br = BufferedReader(FileReader(file))
+                    val br = BufferedReader(FileReader(file))
                     val input = br.readLine()
                     fr.close()
                     br.close()
-                    if(input == "done") startBoot()
-                    else startSetup()
+                    if(input == "done"){
+                        println("Great! Setup file stage at \"done\". Starting boot.")
+                        startBoot()
+                    }
+                    else{
+                        println("Setup seems to have stopped at stage $input. Starting setup.")
+                        startSetup()
+                    }
                 } catch (e: IOException) {
                     file.delete()
                     file.createNewFile()
@@ -66,13 +78,13 @@ class Iona : AppCompatActivity() {
                     }
                     catch(es: SetupFileReadFailedException){
                         es.printStackTrace()
+                        println("Setup file corrupted.")
                     }
+                    println("Starting setup process after failed file read (IOException).")
                     startSetup()
                 }
             }
         }, 5000)
-
-
     }
 }
 
